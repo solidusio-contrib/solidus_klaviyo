6ba0239793f41bec0c09ecaaa4429a93267ac019
Alessandro Desantis <desa.alessandro@gmail.com>
2020-07-20 16:54:17 +0200

    Add missing configuration options to initializer

--- lib/generators/solidus_klaviyo/install/templates/initializer.rb
+++ lib/generators/solidus_klaviyo/install/templates/initializer.rb
@@ -3,4 +3,14 @@
 SolidusKlaviyo.configure do |config|
   # Your Klaviyo API key, in order to communicate with the Klaviyo API.
   config.api_key = 'YOUR_KLAVIYO_API_KEY'
+
+  # A proc that accepts a variant and returns the URL of that variant's PDP.
+  config.variant_url_builder = proc do |variant|
+    "https://example.com/products/#{variant.product.slug}"
+  end
+
+  # A proc that accepts a variant and returns the URL of that variant's main image.
+  config.image_url_builder = proc do |variant|
+    (variant.images.first || variant.product.images.first)&.attachment&.url(:product)
+  end
 end
