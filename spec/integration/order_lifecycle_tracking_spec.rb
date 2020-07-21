4a5d237338cab7903d46540823b7637c66e1fa1e
Alessandro Desantis <desa.alessandro@gmail.com>
2020-07-21 17:24:18 +0200

    Improve integration specs

--- spec/integration/order_lifecycle_tracking_spec.rb
+++ spec/integration/order_lifecycle_tracking_spec.rb
@@ -7,14 +7,22 @@ RSpec.describe 'Order lifecycle tracking' do
       match_requests_on: [:method, VCR.request_matchers.uri_without_param(:data)],
       allow_playback_repeats: true,
     ) do
+      canceler = create(:user)
+
       perform_enqueued_jobs do
+        # Started Checkout
         order = Spree::TestingSupport::OrderWalkthrough.up_to(:payment)
+
+        # Placed Order + Ordered Product
         order.complete!
+
+        # Cancelled Order
+        order.canceled_by(canceler)
       end
     end
 
     expect(
       a_request(:get, /#{Regexp.escape('a.klaviyo.com/crm/api/track')}/)
-    ).to have_been_made.times(3)
+    ).to have_been_made.times(4)
   end
 end
