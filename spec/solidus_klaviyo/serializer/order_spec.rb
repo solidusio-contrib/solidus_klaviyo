0fce407fed8515ce4a15a62befd306e82bbf3453
Alessandro Desantis <desa.alessandro@gmail.com>
2020-07-23 18:29:32 +0200

    Include payment data in order serialization

--- spec/solidus_klaviyo/serializer/order_spec.rb
+++ spec/solidus_klaviyo/serializer/order_spec.rb
@@ -3,7 +3,7 @@
 RSpec.describe SolidusKlaviyo::Serializer::Order do
   describe '.serialize' do
     it 'serializes the order' do
-      order = build_stubbed(:order)
+      order = build_stubbed(:completed_order_with_totals)
 
       expect(described_class.serialize(order)).to be_instance_of(Hash)
     end
