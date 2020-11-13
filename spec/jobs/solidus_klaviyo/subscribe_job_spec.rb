b0faf2d2a347006fbbc59c4c8808b09c30059402
Alessandro Desantis <desa.alessandro@gmail.com>
2020-11-13 12:57:28 +0100

    Add missing test for rate limit management

--- spec/jobs/solidus_klaviyo/subscribe_job_spec.rb
+++ spec/jobs/solidus_klaviyo/subscribe_job_spec.rb
@@ -1,16 +1,40 @@
 # frozen_string_literal: true
 
 RSpec.describe SolidusKlaviyo::SubscribeJob do
-  it 'subscribes the given email to the given list' do
-    solidus_klaviyo = class_spy(SolidusKlaviyo)
-    stub_const('SolidusKlaviyo', solidus_klaviyo)
+  include ActiveSupport::Testing::TimeHelpers
 
-    described_class.perform_now('dummyListId', 'jdoe@example.com', first_name: 'John')
+  context 'when Klaviyo is not rate-limited' do
+    it 'subscribes the given email to the given list' do
+      solidus_klaviyo = class_spy(SolidusKlaviyo)
+      stub_const('SolidusKlaviyo', solidus_klaviyo)
 
-    expect(solidus_klaviyo).to have_received(:subscribe_now).with(
-      'dummyListId',
-      'jdoe@example.com',
-      first_name: 'John',
-    )
+      described_class.perform_now('dummyListId', 'jdoe@example.com', first_name: 'John')
+
+      expect(solidus_klaviyo).to have_received(:subscribe_now).with(
+        'dummyListId',
+        'jdoe@example.com',
+        first_name: 'John',
+      )
+    end
+  end
+
+  context 'when Klaviyo is rate-limited' do
+    it 'reschedules the job for later' do
+      freeze_time do
+        allow(SolidusKlaviyo).to receive(:subscribe_now)
+          .and_raise(SolidusKlaviyo::RateLimitedError.new(OpenStruct.new(
+            headers: { 'retry-after' => 60 },
+            parsed_response: { 'detail' => 'error message' },
+          )))
+
+        described_class.perform_now('dummyListId', 'jdoe@example.com', first_name: 'John')
+
+        expect(described_class).to have_been_enqueued.with(
+          'dummyListId',
+          'jdoe@example.com',
+          first_name: 'John',
+        ).at(60.seconds.from_now)
+      end
+    end
   end
 end
