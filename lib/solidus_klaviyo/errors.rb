c06f8eb51d2b110054e17bfb517cd8340131be9c
Alessandro Desantis <desa.alessandro@gmail.com>
2020-11-13 11:51:54 +0100

    Handle Klaviyo rate-limiting gracefully for subscriptions

--- lib/solidus_klaviyo/errors.rb
+++ lib/solidus_klaviyo/errors.rb
@@ -1,5 +1,24 @@
 # frozen_string_literal: true
 
 module SolidusKlaviyo
-  class SubscriptionError < RuntimeError; end
+  class SubscriptionError < RuntimeError
+    attr_reader :response
+
+    def initialize(response, *args)
+      @response = response
+
+      super(response.parsed_response['detail'], *args)
+    end
+  end
+
+  class RateLimitedError < RuntimeError
+    attr_reader :response, :retry_after
+
+    def initialize(response, *args)
+      @response = response
+      @retry_after = response.headers['retry-after'].to_i.seconds
+
+      super(response.parsed_response['detail'], *args)
+    end
+  end
 end
